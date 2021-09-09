325f480e26119bb1073e169563f22405e55f23ca
Caio Feiertag <caio.feiertag@liberedu.com>
2021-09-09 01:50:04 -0300

    feat: add ticker search

--- lib/screens/home/index.dart
+++ lib/screens/home/index.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:ticker_tracker/screens/home/components/ticker-info.dart';
 import 'package:ticker_tracker/screens/home/ticker.dart';
@@ -12,6 +14,10 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final List<Ticker> tickers = mostValuableTickers;
+  bool isSearching = false;
+  final TextEditingController _controller = TextEditingController();
+  List<Ticker> searchResults = [];
+  Timer? _debounce;
 
   @override
   void initState() {
@@ -22,17 +28,97 @@ class _HomeState extends State<Home> {
   }
 
   @override
+  void dispose() {
+    _debounce?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Home"),
+          title: Row(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: isSearching
+                  ? [
+                      IconButton(
+                        icon: const Icon(Icons.arrow_back),
+                        tooltip: 'Voltar',
+                        onPressed: () {
+                          setState(() {
+                            isSearching = false;
+                          });
+                        },
+                      ),
+                      Expanded(
+                          child: TextField(
+                              controller: _controller,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                hintStyle: TextStyle(color: Colors.white70),
+                                hintText: 'Buscar',
+                                border: InputBorder.none,
+                              ),
+                              onSubmitted: (value) {
+                                setState(() {
+                                  // Searc
+                                });
+                              },
+                              onChanged: (String query) {
+                                if (_debounce?.isActive ?? false) {
+                                  _debounce!.cancel();
+                                }
+                                _debounce = Timer(
+                                    const Duration(milliseconds: 500), () {
+                                  searchTicker(query).then((tickers) {
+                                    setState(() {
+                                      searchResults = tickers;
+                                    });
+                                  });
+                                });
+                              })),
+                    ]
+                  : [
+                      Text("Meu portifólio"),
+                      Spacer(),
+                      IconButton(
+                        icon: const Icon(Icons.search),
+                        tooltip: 'Pressione para começar a pesquisar',
+                        onPressed: () {
+                          setState(() {
+                            isSearching = true;
+                          });
+                        },
+                      ),
+                    ]),
         ),
-        body: ListView.builder(
-            itemCount: tickers.length,
-            itemBuilder: (BuildContext context, int index) {
-              Ticker ticker = tickers[index];
+        body: Stack(children: [
+          Expanded(
+              child: ListView.builder(
+                  itemCount: tickers.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Ticker ticker = tickers[index];
 
-              return TickerInfo(ticker: ticker);
-            }));
+                    return TickerInfo(ticker: ticker);
+                  })),
+          isSearching && _controller.text.length > 0
+              ? Positioned(
+                  left: 0,
+                  top: 0,
+                  child: new Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    decoration: new BoxDecoration(color: Colors.white),
+                    child: ListView.builder(
+                        itemCount: searchResults.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          Ticker ticker = searchResults[index];
+
+                          return TickerInfo(ticker: ticker);
+                        }),
+                  ))
+              : SizedBox.shrink(),
+        ]));
   }
 }
--- lib/screens/home/ticketApi.dart
+++ lib/screens/home/ticketApi.dart
@@ -4,9 +4,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ticker_tracker/screens/home/ticker.dart';
 import 'package:http/http.dart' as http;
 
-Future<Ticker> fetchTicker(Ticker ticker) async {
-  final alphaVantageKey = dotenv.get('ALPHA_VANTAGE_KEY');
+final alphaVantageKey = dotenv.get('ALPHA_VANTAGE_KEY');
 
+Future<Ticker> fetchTicker(Ticker ticker) async {
   final response = await http.get(Uri.parse(
       "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker.code}&apikey=$alphaVantageKey&datatype=json"));
 
@@ -18,5 +18,25 @@ Future<Ticker> fetchTicker(Ticker ticker) async {
     return ticker;
   } else {
     throw Exception("Failed to load ticker ${ticker.code}");
+  }
+}
+
+Future<List<Ticker>> searchTicker(String keywords) async {
+  final response = await http.get(Uri.parse(
+      "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=$keywords&apikey=$alphaVantageKey&datatype=json"));
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    final bestMatches = data["bestMatches"] as List;
+    final List<Ticker> tickers = [];
+
+    bestMatches.forEach((tickerData) => {
+          tickers.add(new Ticker(
+              code: tickerData["1. symbol"], name: tickerData["2. name"]))
+        });
+
+    return tickers;
+  } else {
+    throw Exception("Failed to search tickers");
   }
 }
