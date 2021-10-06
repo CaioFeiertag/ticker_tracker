e6e8d65a1a319e0833513f8cd41457332bc431b7
Caio Feiertag <caio.feiertag@liberedu.com>
2021-10-06 20:46:34 -0300

    use named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ticker_tracker/screens/home/index.dart';
+import 'package:ticker_tracker/screens/ticker-details/index.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
@@ -16,7 +17,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        '/ticker': (context) => TickerDetails(),
+      },
     );
   }
 }
